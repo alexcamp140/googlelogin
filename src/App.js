32eedb2860b7d19f397121b4b3a6465a4193803c
alexcamp140 <alexcamp140@gmail.com>
2024-01-30 22:02:04 +0100

    init repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
   return (
@@ -17,6 +18,15 @@ function App() {
         >
           Learn React
         </a>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+        ;
       </header>
     </div>
   );
