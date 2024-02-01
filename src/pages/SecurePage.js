1c1fa9787f3cadb6f07786042123523dd05dd9a5
alexcamp140 <alexcamp140@gmail.com>
2024-02-01 20:39:13 +0100

    feature logion continue

---
+++ src/pages/SecurePage.js
@@ -0,0 +1,14 @@
+function SecurePage() {
+  return (
+    <div>
+      <h1>Secure Page</h1>
+      <p>
+        You are on a secure page because you are logged in with Google
+        authentication
+      </p>
+      
+    </div>
+  );
+}
+
+export default SecurePage;
